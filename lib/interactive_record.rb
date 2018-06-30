7f3e9f3bce3881f8ad0a6535077127611608a5cf
Maverick Tesini <mavericktesini@gmail.com>
2018-06-30 23:45:38 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,12 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
-class InteractiveRecord
+class InteractiveRecord #will be superclass to student
+
+  #table_name
+  def table_name
+    #table name is class name, lowercase and pluralized(active_support/inflector?)
+    self.to_s.downcase.pluralized
+  end 
   
 end
