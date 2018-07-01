9b49d80f1850d28e9026ba970cd9ba491fe2e9fd
Maverick Tesini <mavericktesini@gmail.com>
2018-07-01 00:29:42 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -45,10 +45,6 @@ class InteractiveRecord #will be superclass to student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = #{name}"
-    DB[:conn].execute(sql)
-  end
 
 
 end
